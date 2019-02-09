f2e7ecb5eb436dcd05c4ab3595376ae524209333
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-02-09 18:33:11 -0200

    less noise, still random

--- prison.py
+++ prison.py
@@ -1,3 +1,5 @@
+import noise
+
 size('A3')
 
 n_w = 9
@@ -26,17 +28,23 @@ for _x in range(n_w):
         fill(1)
         
         colors = {
-            'top': {'r':0.9, 'g':0, 'b':0},
+            'top': {'r':0.6, 'g':0, 'b':0},
             'left': {'r':0.4, 'g':0, 'b':0},
             'right': {'r':0.4, 'g':0, 'b':0},
             'bottom': {'r':0.2, 'g':0, 'b':0},
             'fill': {'r':1, 'g':0, 'b':0},
             }
         
-        frac = 0.6 # % of bigger rect
-        small_s = w * frac        
-        sx0 = x0 + random() * (1 - frac) * w
-        sy0 = y0 + random() * (1 - frac) * h
+        frac = 0.3 # % of bigger rect
+        small_s = w * frac
+        
+        # part perlin, half noise 
+        r1 = 0.7 * abs(noise.pnoise1(_x/n_w, octaves=2)) + 0.3 * random()
+        r2 = 0.7 * abs(noise.pnoise1(_y/n_h, octaves=2)) + 0.3 * random()
+        # print(f'{r1} {r2}')
+        
+        sx0 = x0 + r1 * (1 - frac) * w
+        sy0 = y0 + r2 * (1 - frac) * h
         
         x1 = x0 + w
         y1 = y0 + h
