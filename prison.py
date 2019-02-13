b9fc9c06131f43695ae48120e2624280605e5d46
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-02-13 17:23:46 -0200

    some explaining comments (in case @das_habakuk ever comes here!)

--- prison.py
+++ prison.py
@@ -6,7 +6,7 @@ transparent_bg = True
 # 2 : lines only
 # 3 : 3d boxes
 mode = 3
-s_mode = 6
+s_mode = 4
 mode_adjustment = 1
 
 use_cmyk = True
@@ -43,17 +43,6 @@ elif s_mode == 6: # Youtube cover
     n_w = 80
     use_cmyk = False
     
-n_h = round(n_w * height()/width())
-
-if n_h == 0:
-    n_h = 1
-    aux_n_w = n_w
-    n_w = round(width()/height())
-    print(f'Too few squares to fill this page! \nYou’d need {n_w} squares, so we went ahead and overrode your initial setting of {aux_n_w}!')
-
-w = (width())/n_w
-h = (height())/n_h
-
 if not transparent_bg:
     if use_cmyk:
         cmykFill(0,0,0,1)
@@ -61,35 +50,63 @@ if not transparent_bg:
         fill(0)
     rect(0,0,width(),height())
     
+    
+# how many rows for a given (n_w) number of columns?
+n_h = round(n_w * height()/width())
 
+
+# if the width is too wide, change the number of columns to its minimum
+# value considering square unites
+if n_h == 0:
+    n_h = 1
+    aux_n_w = n_w
+    n_w = round(width()/height())
+    print(f'Too few squares to fill this page! \nYou’d need {n_w} squares, so we went ahead and overrode your initial setting of {aux_n_w}!')
+
+
+# this calculates the width and height of each “square”
+# (which will be as square as possible, but rarely so)
+w = (width())/n_w
+h = (height())/n_h
+    
+
+
+# dras the grid
 for _x in range(n_w):
     for _y in range(n_h):
         
+        # first coordinates of the bigger square
         x0 = _x * w
         y0 = _y * h
         
         # part perlin, half noise 
-        perlin = 0.93
+        perlin = 0.95
         
         rx = perlin * abs(noise.pnoise1(_x/n_w, repeat=n_w)) + (1-perlin) * random()
         ry = perlin * abs(noise.pnoise1(_y/n_h, repeat=n_h)) + (1-perlin) * random()
         
         frac = min([0.9, 0.2 + 0.4 * rx + 0.4 * ry])
         
+        # sometimes i'll need smaller or bigger gaps         
         frac = min([0.9, frac*mode_adjustment])
         
+        # the size of the gap
         small_s = w * frac
         
+        # first coordinates of the smaller square
         sx0 = x0 + rx * (1 - frac) * w
         sy0 = y0 + ry * (1 - frac) * h
         
+        # second coordinates of the bigger square
         x1 = x0 + w
         y1 = y0 + h
         
+        # second coordinates of the smaller square
         sx1 = sx0 + small_s
         sy1 = sy0 + small_s
         
         
+        # only squares
         if mode == 1:
             stroke(1)
             if use_cmyk:
@@ -104,7 +121,8 @@ for _x in range(n_w):
             else:
                 fill(93/255, 5/255, 58/255)
             rect(sx0, sy0, small_s, small_s)
-            
+        
+        # only lines
         if mode == 2:                        
             fill(None)
             if use_cmyk:
@@ -124,6 +142,7 @@ for _x in range(n_w):
             # Bottom
             polygon((x0, y0), (x1, y0), (sx1, sy0), (sx0, sy0), close = True)
         
+        # faux 3d
         if mode == 3:
             
             stroke(None)
